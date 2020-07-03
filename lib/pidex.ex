b5cefd3a5291a462a3aa6819a9770036ff8884be
Hez Ronningen <code@hez.ca>
2020-07-03 16:17:40 -0700

    collapse pid explosion in update/4 definition and run mix format
    Since the bit is a known struct and matching deep in to it makes the function definition quite long it was nice to shrink it down

--- lib/pidex.ex
+++ lib/pidex.ex
@@ -4,10 +4,18 @@ defmodule Pidex do
   """
   require Logger
 
-  defstruct set_point: 0.0, min_point: nil, max_point: nil, kP: 0.0, kI: 0.0, kD: 0.0, ts_factor: 1.0, bias: 0.0, error_power: 1.0
+  defstruct set_point: 0.0,
+            min_point: nil,
+            max_point: nil,
+            kP: 0.0,
+            kI: 0.0,
+            kD: 0.0,
+            ts_factor: 1.0,
+            bias: 0.0,
+            error_power: 1.0
 
   @type timestamp() :: integer | float
-  @type t() :: %Pidex{ }
+  @type t() :: %Pidex{}
 
   @doc """
   Main PID(ex) controller update function. Each call to this will produce a new PID(ex)
@@ -18,50 +26,87 @@ defmodule Pidex do
       {pid, state, 5.0, 1}
       |> Pidex.update()
   """
-  @spec update({Pidex.t(), Pidex.State.t(), number, Pidex.timestamp()}) :: {number, Pidex.State.t()}
+  @spec update({Pidex.t(), Pidex.State.t(), number, Pidex.timestamp()}) ::
+          {number, Pidex.State.t()}
   def update({%Pidex{} = pid, %Pidex.State{} = state, process_value, ts}) do
-   update(pid, state, process_value, ts)
+    update(pid, state, process_value, ts)
   end
 
   @spec update(Pidex.t(), Pidex.State.t(), number, Pidex.timestamp()) :: {number, Pidex.State.t()}
-  def update(%Pidex{kP: kP, kI: kI, kD: kD, set_point: target} = pid,
-             %Pidex.State{} = state, process_value, ts) do
-
-    dT = (ts - state.ts)/pid.ts_factor
+  def update(%Pidex{} = pid, %Pidex.State{} = state, process_value, ts) do
+    dT = (ts - state.ts) / pid.ts_factor
 
     # IO.puts "update: dT: #{inspect dT}, ts: #{ts}, state.ts: #{state.ts}"
-    unless dT > 0, do: raise %ArgumentError{message: "argument error, PID timestep must be non-zero"}
+    unless dT > 0,
+      do: raise(%ArgumentError{message: "argument error, PID timestep must be non-zero"})
 
-    sgn = if (target - process_value) < 0.0 do -1.0 else 1.0 end
-    p_error! = sgn * :math.pow(abs(target - process_value), pid.error_power)
-    p_integral! = state.integral + (p_error! * dT)
+    sgn =
+      if pid.set_point - process_value < 0.0 do
+        -1.0
+      else
+        1.0
+      end
+
+    p_error! = sgn * :math.pow(abs(pid.set_point - process_value), pid.error_power)
+    p_integral! = state.integral + p_error! * dT
     p_derivative! = (p_error! - state.error) / dT
 
-    output = kP*p_error! + kI*p_integral! + kD*p_derivative! + pid.bias
+    output = pid.kP * p_error! + pid.kI * p_integral! + pid.kD * p_derivative! + pid.bias
 
-    Logger.debug("pdx: state: #{inspect state}")
-    Logger.debug("pdx: process: #{inspect [process_value: process_value, set_point: target]}")
-    Logger.debug("pdx: integral: #{inspect [integral: state.integral, p_error_dt: (p_error! * dT)]}")
-    Logger.debug("pdx: calc: #{inspect [ts: ts, ts!: state.ts, del_ts:  ts - state.ts, dT: dT, p_error: p_error!, p_integral: p_integral!, p_derivative: p_derivative!]}")
-    Logger.debug("pdx: form: #{inspect [error: kP*p_error!, integral: kI*p_integral!, derivative: kD*p_derivative!]}")
+    Logger.debug("pdx: state: #{inspect(state)}")
+
+    Logger.debug(
+      "pdx: process: #{inspect(process_value: process_value, set_point: pid.set_point)}"
+    )
+
+    Logger.debug("pdx: integral: #{inspect(integral: state.integral, p_error_dt: p_error! * dT)}")
+
+    Logger.debug(
+      "pdx: calc: #{
+        inspect(
+          ts: ts,
+          ts!: state.ts,
+          del_ts: ts - state.ts,
+          dT: dT,
+          p_error: p_error!,
+          p_integral: p_integral!,
+          p_derivative: p_derivative!
+        )
+      }"
+    )
+
+    Logger.debug(
+      "pdx: form: #{
+        inspect(
+          error: pid.kP * p_error!,
+          integral: pid.kI * p_integral!,
+          derivative: pid.kD * p_derivative!
+        )
+      }"
+    )
 
     # Implement anti-windup & overmax protection
     # cf https://apmonitor.com/pdc/index.php/Main/ProportionalIntegralDerivative
     %{min_point: min_point, max_point: max_point} = pid
+
     {output, p_integral!} =
       case output do
         updated_value when is_number(min_point) and updated_value < min_point ->
           {min_point, p_integral! - p_error! * dT}
+
         updated_value when is_number(max_point) and updated_value > max_point ->
           {max_point, p_integral! - p_error! * dT}
+
         updated_value ->
           {updated_value, p_integral!}
       end
 
-    Logger.debug("pdx: state!: #{inspect %Pidex.State{error: p_error!, integral: p_integral!, ts: ts}}")
+    Logger.debug(
+      "pdx: state!: #{inspect(%Pidex.State{error: p_error!, integral: p_integral!, ts: ts})}"
+    )
+
     {output, %Pidex.State{error: p_error!, integral: p_integral!, ts: ts}}
   end
 end
-
 
 # TroFirmware.PdxRefOrig |> Pidex.PdxServer.set(kP: 3.4e-0)
